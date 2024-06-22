793e9211a9b0b6db0a7e16266ce78e401df804a0
lkereceni <lovro.kereceni@gmail.com>
2024-06-22 13:21:42 +0200

    Create dashboard page grid template

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 
 import styles from "./page.module.css";
+import clsx from "clsx";
 
 const DashboardPage = () => {
-  return <div className="page-container">Dashboard</div>;
+  return (
+    <div className={clsx("page-container", styles["dashboard-grid"])}>
+      <div className={styles["card-one"]}></div>
+      <div className={styles["card-two"]}></div>
+      <div className={styles["recent-transaction"]}></div>
+      <div className={styles["weekly-activity"]}></div>
+      <div className={styles["expense-statistics"]}></div>
+      <div className={styles["quick-transfer"]}></div>
+      <div className={styles["balance-history"]}></div>
+    </div>
+  );
 };
 
 export default DashboardPage;
